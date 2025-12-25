5b4c7211843aaa067d9a64879f322d8c81a25c16
HiteshiPatil10 <hphiteshipatil@gmail.com>
2025-12-26 03:22:58 +0530

    ReLibro-More than a BookStore

--- frontend/src/app/books/add/page.tsx
+++ frontend/src/app/books/add/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import { useEffect, useState } from "react";
 import { apiRequest } from "@/services/api";
 import { useRouter } from "next/navigation";
