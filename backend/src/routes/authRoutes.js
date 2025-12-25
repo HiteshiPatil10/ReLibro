7c54415a988cdea1d0400f67d8faec779c6b27be
HiteshiPatil10 <hphiteshipatil@gmail.com>
2025-12-26 02:22:16 +0530

    ReLibro-More than a BookStore

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -10,3 +10,13 @@ const { login, me } = require("../controllers/authController");
 const { protect } = require("../middlewares/authMiddleware");
 
 router.get("/me", protect, me);
+/*
+import express from "express";
+import { login, me } from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/login", login);
+router.get("/me", me);
+
+export default router;*/
